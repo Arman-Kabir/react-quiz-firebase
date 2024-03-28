3162f78e44e2b54a4b4c23d192b34497ca0da2eb
Arman-Kabir <armankabir8@gmail.com>
2024-03-28 13:11:10 +0600

    signup page css added

---
+++ src/components/pages/Signup.jsx
@@ -0,0 +1,16 @@
+import Form from "../Form";
+import Illustration from "../Illustration";
+import classes from '../../styles/Signup.module.css';
+
+export default function Signup() {
+  return (
+    <>
+      <h1>Create an account</h1>
+
+      <div className="column">
+        <Illustration />
+        <Form className={`${classes.signup} form`} />
+      </div>
+    </>
+  );
+}
