856c2859bb1160ca599807ae8d17bcf213fe79c7
Arman-Kabir <armankabir8@gmail.com>
2024-04-16 09:52:51 +0600

    login illustration done

--- src/components/Illustration.jsx
+++ src/components/Illustration.jsx
@@ -1,11 +1,26 @@
 import classes from "../styles/Illustration.module.css";
 import signupImage from "../assets/images/signup.svg";
+import loginImage from "../assets/images/login.svg";
+
+import { useState, useEffect } from "react";
 
 export default function Illustration() {
+  let [Login, setLogin] = useState(true);
+
+  useEffect(() => {
+    let location = window.location.href;
+    if (location.includes("signup.html")) {
+      setLogin(false);
+    } else {
+      setLogin(true);
+    }
+  },[]);
+
+  console.log(location);
   return (
     <>
       <div className={classes.illustration}>
-        <img src={signupImage} alt="Signup" />
+        <img src={Login ? loginImage : signupImage} alt="Signup" />
       </div>
     </>
   );
---
+++ src/components/pages/Login.jsx
@@ -0,0 +1,14 @@
+import classes from "../../styles/Login.module.css";
+import Illustration from "../Illustration";
+
+export default function Login() {
+  return (
+    <>
+      <h1>Login to your account</h1>
+      <div className="column">
+        <Illustration></Illustration>
+        
+      </div>
+    </>
+  );
+}
