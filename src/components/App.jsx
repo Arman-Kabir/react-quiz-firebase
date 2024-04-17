8fe330a4be8f14f17dc58df3ffee0e2a336226d3
Arman-Kabir <armankabir8@gmail.com>
2024-04-17 15:13:16 +0600

    app.jsx modified to set router:

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,20 +1,29 @@
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import "../styles/App.css";
 import Layout from "./Layout";
-// import Home from "./pages/Home";
-// import Signup from "./pages/Signup";
-// import Login from "./pages/Login";
-// import Quiz from "./pages/Quiz";
+import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
+import Quiz from "./pages/Quiz";
 import Result from "./pages/Result";
 
 function App() {
   return (
-    <Layout>
-      {/* <Home></Home> */}
-      {/* <Signup></Signup> */}
-      {/* <Login></Login> */}
-      {/* <Quiz></Quiz> */}
-      <Result ></Result>
-    </Layout>
+    <Router>
+      <Layout>
+        <Routes>
+          <Route path="/" component={<Home />}></Route>
+
+          <Route path="/signup" component={<Signup />}></Route>
+
+          <Route path="/login" component={<Login />}></Route>
+
+          <Route path="/quiz" component={<Quiz />}></Route>
+
+          <Route path="/result" component={<Result />}></Route>
+        </Routes>
+      </Layout>
+    </Router>
   );
 }
 
