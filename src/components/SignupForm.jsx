7b4292df0d9b6fcb65e9950a7a6e58995af38102
Arman-Kabir <armankabir8@gmail.com>
2024-04-18 11:52:08 +0600

    handleSUbmit and navigation done

--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -1,8 +1,9 @@
+import { useState } from "react";
 import Form from "./Form";
 import TextInput from "./TextInput";
 import Checkbox from "./Checkbox";
 import Button from "./Button";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 import { useAuth } from "../contexts/AuthContext";
 
 export default function SignupForm() {
@@ -12,14 +13,40 @@ export default function SignupForm() {
   const [confirmPassword, setConfirmPassword] = useState("");
   const [agree, setAgree] = useState("");
 
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState();
+
   const { signup } = useAuth();
 
+  const navigate = useNavigate();
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    // do validation
+    if (password !== confirmPassword) {
+      return setError("Password don't match!");
+    }
+
+    try {
+      setError("");
+      setLoading(true);
+      console.log(email, password, username);
+      await signup(email, password, username);
+      navigate("/");
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+      setError("Failed to create a n accounyt!");
+    }
+  }
+
   return (
-    <Form style={{ height: "500px" }}>
+    <Form style={{ height: "500px" }} onSubmit={handleSubmit}>
       <TextInput
         type="text"
         placeholder="Enter Name"
         icon="person"
+        required
         value={username}
         onChange={(e) => setUserName(e.target.value)}
       />
@@ -28,6 +55,7 @@ export default function SignupForm() {
         type="text"
         placeholder="Enter email"
         icon="alternate_email"
+        required
         value={email}
         onChange={(e) => setEmail(e.target.value)}
       />
@@ -36,6 +64,7 @@ export default function SignupForm() {
         type="password"
         placeholder="Enter password"
         icon="lock"
+        required
         value={password}
         onChange={(e) => setPassword(e.target.value)}
       />
@@ -44,19 +73,23 @@ export default function SignupForm() {
         type="password"
         placeholder="Confirm password"
         icon="lock_clock"
+        required
         value={confirmPassword}
         onChange={(e) => setConfirmPassword(e.target.value)}
       />
 
       <Checkbox
         text={`I agree to the Terms  Conditions`}
+        required
         value={agree}
         onChange={(e) => setAgree(e.target.value)}
       />
 
-      <Button>
+      <Button disabled={loading} type="submit">
         <span>Submit Now</span>
       </Button>
+
+      {error && <p className="error">{error}</p>}
 
       <div className="info">
         Already have an account? <Link to="/login">Login</Link> instead.
--- src/firebase.js
+++ src/firebase.js
@@ -3,12 +3,18 @@ import { initializeApp } from 'firebase/app';
 // /firebase configuration
 
 const app = initializeApp({
-    apiKey: "process.env.REACT_APP_apiKey",
-    authDomain: "process.env.REACT_APP_authDomain",
-    projectId: "process.env.REACT_APP_projectId",
-    storageBucket: "process.env.REACT_APP_storageBucket",
-    messagingSenderId: "process.env.REACT_APP_messagingSenderId",
-    appId: "process.env.REACT_APP_appId"
+    apiKey: "AIzaSyBwo8rcFCc9xYLa1Xkh3CF3hp3StAvcu8k",
+    authDomain: "react-quiz-dev-53210.firebaseapp.com",
+    projectId: "react-quiz-dev-53210",
+    storageBucket: "react-quiz-dev-53210.appspot.com",
+    messagingSenderId: "827027098914",
+    appId: "1:827027098914:web:9c829640b082596ca5e97b"
+    // apiKey: "process.env.REACT_APP_API_KEY",
+    // authDomain: "process.env.REACT_APP_AUTH_DOMAIN",
+    // projectId: "process.env.REACT_APP_PROJECT_ID",
+    // storageBucket: "process.env.REACT_APP_STORAGE_BUCKET",
+    // messagingSenderId: "process.env.REACT_APP_MESSAGING_SENDER_ID",
+    // appId: "process.env.REACT_APP_ID"
 });
 
 export default app;
