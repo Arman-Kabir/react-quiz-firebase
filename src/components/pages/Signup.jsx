e19c54204a7fe88000de23803c172425fb0edd81
Arman-Kabir <armankabir8@gmail.com>
2024-04-18 10:39:04 +0600

    SignupFOrm refactored

--- src/components/pages/Signup.jsx
+++ src/components/pages/Signup.jsx
@@ -1,10 +1,7 @@
-import Form from "../Form";
-import classes from "../../styles/Signup.module.css";
+
 import Illustration from "../Illustration";
-import TextInput from "../TextInput";
-import Checkbox from "../Checkbox";
-import Button from "../Button";
-import {Link} from 'react-router-dom';
+
+import SignupForm from "../SignupForm";
 
 export default function Signup() {
   return (
@@ -13,34 +10,7 @@ export default function Signup() {
 
       <div className="column">
         <Illustration />
-
-        <Form className={`${classes.signup}`}>
-          <TextInput type="text" placeholder="Enter Name" icon="person" />
-
-          <TextInput
-            type="text"
-            placeholder="Enter email"
-            icon="alternate_email"
-          />
-
-          <TextInput type="password" placeholder="Enter password" icon="lock" />
-
-          <TextInput
-            type="password"
-            placeholder="Confirm password"
-            icon="lock_clock"
-          />
-
-          <Checkbox text={`I agree to the Terms  Conditions`} />
-
-          <Button>
-            <span>Submit Now</span>
-          </Button>
-
-          <div className="info">
-            Already have an account? <Link to="/login">Login</Link> instead.
-          </div>
-        </Form>
+        <SignupForm></SignupForm>
       </div>
     </>
   );
