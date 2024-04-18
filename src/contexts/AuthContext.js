8db393080166b33725a367749ae51f7d1f7a3c94
Arman-Kabir <armankabir8@gmail.com>
2024-04-18 07:25:01 +0600

    firebase auth functionality added

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,13 +1,57 @@
 import React, { useState } from 'react';
+import "../firebase";
+import { getAuth, createUserWithEmailAndPassword, updateProfile, signInWithEmailAndPassword,signOut } from "firebase/auth";
 
 const AuthContext = React.createContext();
 
+export function useAuth() {
+    return useContext(AuthContext);
+}
+
 
 export function AuthProvider({ children }) {
-    const [loading, setLoading] = useState(false);
+    const [loading, setLoading] = useState(true);
+    const [currentUser, setCurrentUser] = useState();
+
+    // signup function
+    async function signup(email, password, username) {
+        const auth = getAuth();
+        await createUserWithEmailAndPassword(auth, email, password);
+
+        // update profile
+        await updateProfile(auth.currentUser, {
+            displayName: username
+        });
+
+        const user = auth.currentUser;
+        setCurrentUser({
+            ...user,
+        })
+    }
+
+    // login function
+    async function login(email, password) {
+        const auth = getAuth();
+        return signInWithEmailAndPassword(auth, email, password);
+    }
+
+    // logout
+    function logout(){
+        const auth = getAuth();
+        return signOut(auth);
+    }
+
+    const value = {
+        currentUser,
+        signup,
+        login,
+        logout,
+    }
+
+
     return (
         <AuthContext.Provider value={value}>
-            {children}
+            {!loading && children}
         </AuthContext.Provider>
     );
 }
