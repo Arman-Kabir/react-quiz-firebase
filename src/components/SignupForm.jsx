f4ea1ac036024b848d4d377bb4fb8d0b82507286
Arman-Kabir <armankabir8@gmail.com>
2024-04-18 11:15:22 +0600

    SignupFOrm input states created

--- src/components/SignupForm.jsx
+++ src/components/SignupForm.jsx
@@ -3,23 +3,56 @@ import TextInput from "./TextInput";
 import Checkbox from "./Checkbox";
 import Button from "./Button";
 import { Link } from "react-router-dom";
+import { useAuth } from "../contexts/AuthContext";
 
 export default function SignupForm() {
+  const [username, setUserName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [agree, setAgree] = useState("");
+
+  const { signup } = useAuth();
+
   return (
     <Form style={{ height: "500px" }}>
-      <TextInput type="text" placeholder="Enter Name" icon="person" />
+      <TextInput
+        type="text"
+        placeholder="Enter Name"
+        icon="person"
+        value={username}
+        onChange={(e) => setUserName(e.target.value)}
+      />
 
-      <TextInput type="text" placeholder="Enter email" icon="alternate_email" />
+      <TextInput
+        type="text"
+        placeholder="Enter email"
+        icon="alternate_email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+      />
 
-      <TextInput type="password" placeholder="Enter password" icon="lock" />
+      <TextInput
+        type="password"
+        placeholder="Enter password"
+        icon="lock"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+      />
 
       <TextInput
         type="password"
         placeholder="Confirm password"
         icon="lock_clock"
+        value={confirmPassword}
+        onChange={(e) => setConfirmPassword(e.target.value)}
       />
 
-      <Checkbox text={`I agree to the Terms  Conditions`} />
+      <Checkbox
+        text={`I agree to the Terms  Conditions`}
+        value={agree}
+        onChange={(e) => setAgree(e.target.value)}
+      />
 
       <Button>
         <span>Submit Now</span>
